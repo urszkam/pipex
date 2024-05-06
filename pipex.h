6e18cc25294ee858bfeff710c38babe8cdba0196
ula kaminska <ursz.kam@gmail.com>
2024-05-06 23:58:35 +0200

    update functions prototypes

--- pipex.h
+++ pipex.h
@@ -6,7 +6,14 @@
 # include <sys/wait.h>
 # include <errno.h>
 # include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
 
 char    **ft_split(char const *s, char c);
+char	*ft_strndup(char const *str, int n);
+char    **create_arr(char *str, char *str2);
+char    *create_path(char *cmd);
+
+void    free_array(char **arr);
 
 #endif
