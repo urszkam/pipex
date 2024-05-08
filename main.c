62323baa85e2075870ad72d4a499992855ed9972
ula kaminska <ursz.kam@gmail.com>
2024-05-08 22:54:47 +0200

    refactor: remove option for here_doc

--- main.c
+++ main.c
@@ -2,13 +2,8 @@ int main(int argc, char **argv)
 {
     if (argc >= 5) 
     {
-        if (strcmp(argv[1], "here_doc") == 0)
-            printf("here_doc");
-        else 
-        {
-            argv++;
-            pipex(argv, argc - 1);
-        }
+        argv++;
+        pipex(argv, argc - 1);
     }
     return (0);
 }
