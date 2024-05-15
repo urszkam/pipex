06a2cb72f671016e9348c8db2404ef9b9d99f109
ula kaminska <ursz.kam@gmail.com>
2024-05-15 22:06:30 +0200

    chore: update prototypes

--- pipex.h
+++ pipex.h
@@ -12,9 +12,12 @@
 char    **ft_split(char const *s, char c);
 char	*ft_strndup(char const *str, int n);
 char    **create_arr(char *str, char *str2);
+char    **create_cmd_vector(char *cmd, int j);
 char    *create_path(char *cmd);
 
 void    free_array(char **arr);
-void    pipex(char **cmd, int cmd_len);
+void    exec_cmd(char *cmd, int j);
+void    close_fds(int *fd, int cmd_len);
+void    open_fds(int *fd, int j, int cmd_len, int output_fd);
 
 #endif
