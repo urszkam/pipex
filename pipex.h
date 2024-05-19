3d3c725f9c15a6abc863dc688a8b17c6fb280ea1
ula kaminska <ursz.kam@gmail.com>
2024-05-20 00:00:51 +0200

    style: change spaces to tabs

--- pipex.h
+++ pipex.h
@@ -9,15 +9,15 @@
 # include <string.h>
 # include <fcntl.h>
 
-char    **ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 char	*ft_strndup(char const *str, int n);
-char    **create_arr(char *str, char *str2);
-char    **create_cmd_vector(char *cmd, int j);
-char    *create_path(char *cmd);
+char	**create_arr(char *str, char *str2);
+char	**create_cmd_vector(char *cmd, int j);
+char	*create_path(char *cmd);
 
-void    free_array(char **arr);
-void    exec_cmd(char *cmd, int j);
-void    close_fds(int *fd, int cmd_len);
-void    open_fds(int *fd, int j, int cmd_len, int output_fd);
+void	free_array(char **arr);
+void	exec_cmd(char *cmd, int j);
+void	close_fds(int *fd, int cmd_len);
+void	open_fds(int *fd, int j, int cmd_len, int output_fd);
 
 #endif
--- pipex_utils.c
+++ pipex_utils.c
@@ -1,75 +1,83 @@
 #include "pipex.h"
 
-void    close_fds(int *fd, int cmd_len) 
+void	close_fds(int *fd, int cmd_len)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < 2 * cmd_len)
-    {
-        if (close(fd[i]) < 0)
-        {
-            perror("close");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
+	i = 0;
+	while (i < 2 * cmd_len)
+	{
+		if (close(fd[i]) < 0)
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
 }
 
-char    **create_cmd_vector(char *cmd, int j)
+char	**create_cmd_vector(char *cmd, int j)
 {
-    char    **cmdv;
+	char	**cmdv;
 
-    if (j == 0)
-        cmdv = create_arr("<", cmd);
-    else
-        cmdv = ft_split(cmd, ' ');
-    return (cmdv);
+	if (j == 0)
+		cmdv = create_arr("cat", cmd);
+	else
+		cmdv = ft_split(cmd, ' ');
+	return (cmdv);
 }
 
-void    open_fds(int *fd, int j, int cmd_len, int output_fd)
+void	open_fds(int *fd, int j, int cmd_len, int output_fd)
 {
-    if (j + 2 < 2 * cmd_len)
-    {
-        if (dup2(fd[j + 1], 1) < 0)
-        {
-            perror("dup2");
-            exit(EXIT_FAILURE);
-        }
-    }
-    if (j > 0)
-    {
-        if (dup2(fd[j - 2], 0) < 0)
-        {
-            perror("dup2");
-            exit(EXIT_FAILURE);
-        }
-    }
-    if (j + 2 == 2 * cmd_len)
-    {
-        if (dup2(output_fd, 1) < 0)
-        {
-            perror("dup2");
-            exit(EXIT_FAILURE);
-        }
-    }
+	if (j + 2 < 2 * cmd_len)
+	{
+		if (dup2(fd[j + 1], 1) < 0)
+		{
+			perror("dup2");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (j > 0)
+	{
+		if (dup2(fd[j - 2], 0) < 0)
+		{
+			perror("dup2");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (j + 2 == 2 * cmd_len)
+	{
+		if (dup2(output_fd, 1) < 0)
+		{
+			perror("dup2");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
-void    exec_cmd(char *cmd, int j)
+void	exec_cmd(char *cmd, int j)
 {
-    char    **cmdv;
-    char    *path;
+	char	**cmdv;
+	char	*path;
 
-    cmdv = create_cmd_vector(cmd, j);
-    if (j == 0)
-        path = ft_strndup("/usr/bin/cat", 15);
-    else 
-        path = create_path(cmdv[0]);
-    if (execve(path, cmdv, NULL) < 0)
-    {
-        perror(cmd);
-        free(path);
-        free_array(cmdv);
-        exit(EXIT_FAILURE);
-    }
+	cmdv = create_cmd_vector(cmd, j);
+	if (j == 0)
+		path = ft_strndup("/usr/bin/cat", 15);
+	else
+		path = create_path(cmdv[0]);
+	if (access(path, X_OK) != 0)
+	{
+		write(2, cmdv[0], strlen(cmdv[0]));
+		write(2, ": command not found\n", 20);
+		free(path);
+		free_array(cmdv);
+		exit(EXIT_FAILURE);
+	}
+	if (execve(path, cmdv, NULL) < 0)
+	{
+		perror(cmdv[0]);
+		free(path);
+		free_array(cmdv);
+		exit(EXIT_FAILURE);
+	}
 }
