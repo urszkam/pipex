90de7a38b64c8e90dfa3c37987a25e742f768089
ula kaminska <ursz.kam@gmail.com>
2024-07-03 20:53:00 +0200

    style: spaces to tabs

--- utils.c
+++ utils.c
@@ -1,29 +1,30 @@
 #include "pipex.h"
 
-int ft_strcmp(char **s1, char **s2)
+int	ft_strcmp(char **s1, char **s2)
 {
-    while (*s1 && *s2)
-    {
-        s1++;
-        s2++;
-    }
-    return (!s1 && !s2);
+	while (*s1 && *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (!s1 && !s2);
 }
 
-void    free_array(char **arr)
+void	free_array(char **arr)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (arr)
-    {
-        while (arr[i]) {
-            free(arr[i]);
-            i++;
-        }
-        free(arr);
-        arr = NULL;
-    }
+	i = 0;
+	if (arr)
+	{
+		while (arr[i])
+		{
+			free(arr[i]);
+			i++;
+		}
+		free(arr);
+		arr = NULL;
+	}
 }
 
 char	*ft_strndup(char const *str, int n)
@@ -44,42 +45,42 @@ char	*ft_strndup(char const *str, int n)
 	return (new);
 }
 
-char    **create_arr(char *str, char *str2)
+char	**create_arr(char *str, char *str2)
 {
-    char    **arr;
+	char	**arr;
 
-    arr = (char **) malloc(3 * sizeof(char *));
-    if (!arr)
-        return (0);
-    arr[0] = ft_strndup(str, strlen(str));
-    arr[1] = ft_strndup(str2, strlen(str2));
-    arr[2] = NULL;
-    return (arr);
+	arr = (char **) malloc(3 * sizeof(char *));
+	if (!arr)
+		return (0);
+	arr[0] = ft_strndup(str, strlen(str));
+	arr[1] = ft_strndup(str2, strlen(str2));
+	arr[2] = NULL;
+	return (arr);
 }
 
-char    *create_path(char *cmd)
+char	*create_path(char *cmd)
 {
-    char    *path;
-    char    *dir;
-    int     i;
-    int     j;
+	char	*path;
+	char	*dir;
+	int		i;
+	int		j;
 
-    dir = "/usr/bin/";
-    path = malloc(strlen(cmd) + strlen(dir) + 1);
-    if (!path)
-        return (0);
-    i = 0;
-    while (dir[i])
-    {
-        path[i] = dir[i];
-        i++;
-    }
-    j = 0;
-    while (cmd[j])
-    {
-        path[i + j] = cmd[j];
-        j++;
-    }
-    path[i + j] = '\0';
-    return (path);
+	dir = "/usr/bin/";
+	path = malloc(strlen(cmd) + strlen(dir) + 1);
+	if (!path)
+		return (0);
+	i = 0;
+	while (dir[i])
+	{
+		path[i] = dir[i];
+		i++;
+	}
+	j = 0;
+	while (cmd[j])
+	{
+		path[i + j] = cmd[j];
+		j++;
+	}
+	path[i + j] = '\0';
+	return (path);
 }
